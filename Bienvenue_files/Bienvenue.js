ba408db053033dd6b2a56f677c048953a9208b95
Adrien Missemer <adrien.missemer@hybris.com>
2015-09-13 12:23:26 -0400

    initial from oh missemer.org site

---
+++ Bienvenue_files/Bienvenue.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20080927
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Bienvenue_files/BienvenueMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();IMpreload('Bienvenue_files','shapeimage_3','0');fixupIECSS3Opacity('id5');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Cousinades_Missemer_files/Cousinades_Missemer.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20080927
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,202),url:'Cousinades_Missemer_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Cousinades_Missemer_files/stroke_1.png'},{rect:new IWRect(1,-1,202,2),url:'Cousinades_Missemer_files/stroke_2.png'},{rect:new IWRect(203,-1,2,2),url:'Cousinades_Missemer_files/stroke_3.png'},{rect:new IWRect(203,1,2,202),url:'Cousinades_Missemer_files/stroke_4.png'},{rect:new IWRect(203,203,2,2),url:'Cousinades_Missemer_files/stroke_5.png'},{rect:new IWRect(1,203,202,2),url:'Cousinades_Missemer_files/stroke_6.png'},{rect:new IWRect(-1,203,2,2),url:'Cousinades_Missemer_files/stroke_7.png'}],new IWSize(204,204))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Cousinades_Missemer_files/Cousinades_MissemerMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();IMpreload('Cousinades_Missemer_files','shapeimage_2','0');IMpreload('Cousinades_Missemer_files','shapeimage_4','0');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
